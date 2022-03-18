0010eda87497e43a5978c5da562aa16d838b5380
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-03-18 17:01:36 -0300

    Finalizando a logica javascript exibindo os cards de acordo com os pokemons que estão selecionados, modificando o index da pagina inicial para o github pages

--- js/index.js
+++ js/index.js
@@ -2,22 +2,27 @@
 
 const listSelectedPokemon = document.querySelectorAll('.pokemon-menu')
 const pokemonSelectedCard = document.querySelectorAll('.card-pokemon ')
-console.log(listSelectedPokemon);
+
 
 
 
 listSelectedPokemon.forEach(pokemon => {
     pokemon.addEventListener('click',()=>{
-        const openCardPokemon = document.querySelector('.open')
-        //Remover a classe active do pokemon que está aberta para realizar a troca do pokemon
-        openCardPokemon.classList.remove('open')
+    const openCardPokemon = document.querySelector('.open')
+    //Remover a classe active do pokemon que está aberta para realizar a troca do pokemon
+    openCardPokemon.classList.remove('open')
 
     const selectedPokemonId = pokemon.attributes.id.value
     const cardPokemonOpen = document.getElementById(`card-${selectedPokemonId}`)
     cardPokemonOpen.classList.add('open')
-    console.log(cardPokemonOpen)
+
+    const cardSelected = document.querySelector('.active')
+    cardSelected.classList.remove('active')
 
 
+    const cardSelectedActive = document.getElementById(selectedPokemonId)
+    cardSelectedActive.classList.add('active')
 
     });
 })
+
